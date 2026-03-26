a2f152c7c29a181ddf3687d97a30f96b8dc044dd
aashby13 <dev@adamashhby.com>
2026-03-25 22:14:36 -0700

    Menu handle resize

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,6 +1,6 @@
 import { NavLink, useLocation } from 'react-router';
 import styles from './Menu.module.scss'
-import { useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 
@@ -10,31 +10,44 @@ export default function Menu() {
   const { pathname } = useLocation();
   const lineRef = useRef<HTMLDivElement>(null);
   const containerRef = useRef<HTMLDivElement>(null);
+  const [resizeCount, setResizeCount] = useState(0);
 
-
-  useGSAP(() => {
+  const getWidthX = useCallback((): { width: number, x: number } => {
+    const data = { width: 0, x: 0 };
     if (containerRef.current) {
       const activeLink: HTMLElement | null = containerRef.current.querySelector('a.active');
       if (activeLink && lineRef.current) {
-        const w = activeLink.clientWidth;
-        const x = activeLink.offsetLeft;
-        gsap.timeline()
-          .to(lineRef.current, { duration: 0.2, width: w * 0.2, ease: 'power3.out'}, 0)
-          .to(lineRef.current, { duration: 0.25, width: w, ease: 'back.out' })
-          .to(lineRef.current, { duration: 0.45, x: x, ease: 'back.out' }, 0);
+        data.width = activeLink.clientWidth;
+        data.x = activeLink.offsetLeft;
       }
     }
-  }, { dependencies: [pathname, lineRef, containerRef]})
+    return data;
+  }, [containerRef])
 
-  /* useEffect(() => {
-    console.log('Menu', pathname, containerRef);
-  }, [pathname, containerRef]); */
+  useGSAP(() => {
+    const { width, x } = getWidthX();
+    if (width && lineRef.current) {
+      gsap.timeline()
+        .to(lineRef.current, { duration: 0.2, width: width * 0.2, ease: 'power3.out'}, 0)
+        .to(lineRef.current, { duration: 0.25, width, ease: 'back.out' })
+        .to(lineRef.current, { duration: 0.45, x, ease: 'back.out' }, 0);
+    }
+  }, { dependencies: [pathname, lineRef, getWidthX]});
+
+  useGSAP(() => {
+    if (resizeCount) {
+      const { width, x } = getWidthX();
+      if (width && lineRef.current) {
+        gsap.set(lineRef.current, { width, x});
+      }
+    }
+    
+  }, { dependencies: [resizeCount]});
 
   useEffect(() => {
     const handleResize = () => {
-        
-    };
-
+      setResizeCount(prev => prev + 1);
+    }
     window.addEventListener("resize", handleResize);
     return () => {
         window.removeEventListener("resize", handleResize);
@@ -46,26 +59,17 @@ export default function Menu() {
       <nav>
         <ul>
           <li>
-            <NavLink 
-              to="/portfolio" 
-              /* ref={linkRefCallback} */
-            >
+            <NavLink to="/portfolio">
               Portfolio
             </NavLink>
           </li>
           <li>
-            <NavLink 
-              to="/about"
-              /* ref={linkRefCallback} */
-            >
+            <NavLink to="/about">
               About
             </NavLink>
           </li>
           <li>
-            <NavLink 
-              to="/contact"
-              /* ref={linkRefCallback} */
-            >
+            <NavLink to="/contact">
               Contact
             </NavLink>
           </li>
