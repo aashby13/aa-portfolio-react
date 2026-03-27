767e0f1491fa87d5c4910bab906715f7d06af80b
aashby13 <dev@adamashhby.com>
2026-03-26 21:56:05 -0700

    Portfolio: nav/redirect to first project

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,7 +1,5 @@
-/* eslint-disable react-hooks/refs */
-
 import { createPortal } from 'react-dom';
-import { useLoaderData, useOutletContext } from "react-router";
+import { Outlet, useLoaderData, useNavigate, useOutletContext, useParams } from "react-router";
 import type { OutletContextDomEls, ProjectJsonData } from "../lib/types";
 import DotNav from '../components/DotNav/DotNav';
 import { useEffect } from 'react';
@@ -9,32 +7,37 @@ import ImageScroll from '../components/ImageScroll/ImageScroll';
 import PlayerLaunchBtn from '../components/PlayerLaunchBtn/PlayerLaunchBtn';
 
 export default function Portfolio() {
-  const { projects, roles, types } = useLoaderData<ProjectJsonData>();
+  const { pid } = useParams();
+  const navigate = useNavigate();
+  const data = useLoaderData<ProjectJsonData>();
   const { 
     gutterEl,
     columnMidEl,
     columnBottomEl 
   } = useOutletContext<OutletContextDomEls>();
 
-  /* useEffect(() => {
-    console.log(gutterRef);
-  }, [gutterRef]) */
+  useEffect(() => {
+    if (!pid) {
+      navigate(`/portfolio/${data.projects[0].id}`, { replace: true });
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pid])
   
 
   return (
     <>
-      <div>Portfolio</div>
+      <Outlet context={data}/>
 
       {
-        !!columnMidEl && createPortal(<ImageScroll projects={projects}/>, columnMidEl)
+        !!columnMidEl && createPortal(<ImageScroll projects={data.projects}/>, columnMidEl)
       }
 
       {
-        !!columnBottomEl && createPortal(<PlayerLaunchBtn projects={projects}/>, columnBottomEl)
+        !!columnBottomEl && createPortal(<PlayerLaunchBtn projects={data.projects}/>, columnBottomEl)
       }
 
       {
-        !!gutterEl && createPortal(<DotNav projects={projects}/>, gutterEl)
+        !!gutterEl && createPortal(<DotNav projects={data.projects}/>, gutterEl)
       }
     </>
     
